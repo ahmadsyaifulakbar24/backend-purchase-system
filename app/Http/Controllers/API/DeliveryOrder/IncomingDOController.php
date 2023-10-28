f64bd210298007564698a6d4ffc5fe5a8f4634ac
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2023-10-28 14:10:47 +0700

    add master file delivery order

---
+++ app/Http/Controllers/API/DeliveryOrder/IncomingDOController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\DeliveryOrder;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IncomingDOController extends Controller
+{
+    //
+}
