3344d2bc8fa54fe602ce41cb818ca8ab1322e7c7
faldibule <faldibule@gmail.com>
2023-11-09 22:41:01 +0700

    Add(UI): add UI for export daily meal sheet & monthly meal sheet

--- resources/views/pdf/daily_meal_sheet.blade.php
+++ resources/views/pdf/daily_meal_sheet.blade.php
@@ -1,12 +1,234 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style type="text/css">
+        .tg {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        .tg td {
+            border-color: black;
+            border-style: solid;
+            border-width: 1px;
+            font-family: Arial, sans-serif;
+            font-size: 11px;
+            overflow: hidden;
+            padding: 5px 5px;
+            font-weight: bold;
+            word-break: normal;
+        }
+
+        .tg th {
+            border-color: black;
+            border-style: solid;
+            border-width: 1px;
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            font-weight: normal;
+            overflow: hidden;
+            padding: 5px 5px;
+            word-break: normal;
+        }
+
+        .tg .tg-baqh {
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-c3ow {
+            border-color: inherit;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-7btt {
+            border-color: inherit;
+            font-weight: bold;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-sn4r {
+            border-color: #333333;
+            font-weight: bold;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-de2y {
+            border-color: #333333;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-dvpl {
+            border-color: inherit;
+            text-align: right;
+            vertical-align: top
+        }
+
+        .tg .tg-fymr {
+            border-color: inherit;
+            font-weight: bold;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-0pky {
+            border-color: inherit;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-0lax {
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .text-left {
+            text-align: center;
+            vertical-align: top;
+            text-align: left;
+        }
+
+        .no {
+            width: 10px;
+        }
+        .br-none {
+            border-right: none;
+        }
+        .bl-none {
+            border-left: none;
+        }
+        .bt-none {
+            border-top: none;
+        }
+        .bb-none {
+            border-bottom: none;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Daily Meal Sheet</h1>
+    <table class="tg" style="width: 700px;">
+        <thead>
+            <tr>
+                <th class="tg-c3ow" colspan="10">
+                    <h1>PT.Surya Buana Lestarijaya</h1>
+                    <b>Catering and Accomodation Service</b>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="tg-7btt" colspan="10" style="border-bottom: none">Meal Count Sheet and Accomodation Record</td>
+            </tr>
+            <tr>
+                <td class="tg-sn4r" colspan="3" style="border-right: none; border-top: none">Kapal 1</td>
+                <td class="tg-de2y" style="border-left: none; border-right: none; border-top: none">Andromeda</td>
+                <td class="tg-dvpl" colspan="6" style="border-left: none; border-top: none">DATE : 25 Februari 2023</td>
+            </tr>
+            <tr>
+                <td class="tg-7btt" style="width: 10px;">NO</td>
+                <td class="tg-7btt">NAME</td>
+                <td class="tg-7btt">POSITION</td>
+                <td class="tg-7btt">COMPANY</td>
+                <td class="tg-7btt">B</td>
+                <td class="tg-7btt">L</td>
+                <td class="tg-7btt">D</td>
+                <td class="tg-7btt">S</td>
+                <td class="tg-7btt">TOTAL</td>
+                <td class="tg-7btt">ACCOM</td>
+            </tr>
+            <tr>
+                <td class="tg-7btt" style="width: 5px;">1</td>
+                <td class="text-center">Faldi Nur Ikhsan</td>
+                <td class="text-center">Teriminal Manager</td>
+                <td class="text-center">ANDROMEDA</td>
+                <td class="tg-0pky">1</td>
+                <td class="tg-0pky">0</td>
+                <td class="tg-0pky">1</td>
+                <td class="tg-0pky">0</td>
+                <td class="tg-0pky">2</td>
+                <td class="tg-0pky">1</td>
+            </tr>
+            <tr>
+                <td class="tg-7btt" style="width: 5px;">2</td>
+                <td class="text-center">Ahmad Syaiful Akbar</td>
+                <td class="text-center">Terminal Manager</td>
+                <td class="text-center">SILLO MARITIME</td>
+                <td class="tg-0lax">1</td>
+                <td class="tg-0lax">1</td>
+                <td class="tg-0lax">1</td>
+                <td class="tg-0lax">1</td>
+                <td class="tg-0lax">4</td>
+                <td class="tg-0lax">1</td>
+            </tr>
+            <tr>
+                <td class="tg-7btt" style="width: 5px;"></td>
+                <td class="tg-7btt">Total</td>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax">2</td>
+                <td class="tg-0lax">1</td>
+                <td class="tg-0lax">2</td>
+                <td class="tg-0lax">1</td>
+                <td class="tg-0lax">6</td>
+                <td class="tg-0lax">2</td>
+            </tr>
+            <tr>
+                <td style="border-right: none; border-bottom: none;"></td>
+                <td style="border-left: none; border-right: none; border-bottom: none; text-align: right; font-weight: bold; font-size: 0.5rem">
+                    <div>Mandays : 27</div>
+                    <div>Casual B Fast : 3</div>
+                    <div>Casual Lunch : 3</div>
+                </td>
+                <td style="border-left: none; border-bottom: none;" colspan="8"></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="border-top: none; border-right: none;">
+                    <div style="min-height: 100px; text-align: center; margin-top: -15px;">
+                        <p>Prepared By,</p>
+                        <p style="margin-top: 60px;">Name</p>
+                        <p style="margin-top: -15px; margin-bottom: 0px;">Position</p>
+                    </div>
+                </td>
+                <td colspan="2" style="border-top: none; border-right: none; border-left: none;">
+                    <div style="min-height: 100px; text-align: center; margin-top: -15px;">
+                        <p>Checked By,</p>
+                        <p style="margin-top: 60px;">Name</p>
+                        <p style="margin-top: -15px; margin-bottom: 0px;">Position</p>
+                    </div>
+                </td>
+                <td colspan="4" style="border-top: none; border-right: none; border-left: none;">
+                    <div style="margin-left: -50px; min-height: 100px; text-align: center; margin-top: -15px;">
+                        <p>Approved By,</p>
+                        <p style="margin-top: 60px;">Name</p>
+                        <p style="margin-top: -15px; margin-bottom: 0px;">Position</p>
+                    </div>
+                </td>
+                <td colspan="2" style="border-top: none; border-left: none;">
+                    <div style="min-height: 100px; text-align: center; margin-top: -15px;">
+                        <p>Acknowledge By,</p>
+                        <p style="margin-top: 60px;">Name</p>
+                        <p style="margin-top: -15px; margin-bottom: 0px;">Position</p>
+                    </div>
+                </td>
+            </tr>
+            <tr style="line-height: 10px; padding: 0;">
+                <td colspan="10" style="text-align: center; background-color: #bcbcbc;">
+                    <span style="font-size: 0.6rem; font-weight: bold;">PT SURYA BUANA TESTARUAYA</span> <br>
+                    <span style="font-size: 0.6rem;">Kompl€k Gading Bukit lndah Blok J No,07 Jalan Bukit Gading Raya RT 018 RW 008 Kelapa Gading.lakarta Utara 142 rt 0</span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </body>
+
 </html>
--- resources/views/pdf/monthly_meal_sheet.blade.php
+++ resources/views/pdf/monthly_meal_sheet.blade.php
@@ -1,12 +1,257 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style type="text/css">
+        .tg {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        .tg td {
+            border-color: black;
+            border-style: solid;
+            border-width: 1px;
+            font-family: Arial, sans-serif;
+            font-size: 11px;
+            overflow: hidden;
+            padding: 5px 5px;
+            font-weight: bold;
+            word-break: normal;
+        }
+
+        .tg th {
+            border-color: black;
+            border-style: solid;
+            border-width: 1px;
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            font-weight: normal;
+            overflow: hidden;
+            padding: 10px 5px;
+            word-break: normal;
+        }
+
+        .tg .tg-baqh {
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-c3ow {
+            border-color: inherit;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-7btt {
+            border-color: inherit;
+            font-weight: bold;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-sn4r {
+            border-color: #333333;
+            font-weight: bold;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-de2y {
+            border-color: #333333;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-dvpl {
+            border-color: inherit;
+            text-align: right;
+            vertical-align: top
+        }
+
+        .tg .tg-fymr {
+            border-color: inherit;
+            font-weight: bold;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-0pky {
+            border-color: inherit;
+            text-align: center;
+            vertical-align: top
+        }
+
+        .tg .tg-0lax {
+            text-align: left;
+            vertical-align: top
+        }
+        .tg .text-left {
+            text-align: center;
+            vertical-align: top;
+            text-align: left;
+        }
+        .tg .text-center {
+            text-align: center;
+            vertical-align: top;
+            text-align: center;
+        }
+        .no {
+            width: 10px;
+        }
+        .br-none {
+            border-right: none;
+        }
+        .bl-none {
+            border-left: none;
+        }
+        .bt-none {
+            border-top: none;
+        }
+        .bb-none {
+            border-bottom: none;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Monthly Meal Sheet</h1>
+    <table class="tg" style="width: 700px;">
+        <thead>
+            <tr>
+                <th class="tg-c3ow" colspan="12">
+                    <h1>PT.Surya Buana Lestarijaya</h1>
+                    <b>Catering and Accomodation Service</b>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="tg-7btt" colspan="12" style="border-bottom: none">
+                    <h2>Summary Meal Count Sheet</h2>
+                </td>
+            </tr>
+            <tr>
+                <td class="tg-sn4r" colspan="3" style="border-right: none;"> Location : Kapal 1</td>
+                <td class="tg-de2y" style="border-left: none; border-right: none;"></td>
+                <td class="tg-dvpl" colspan="8" style="border-left: none;">MONTH: FEBRUARI 2023</td>
+            </tr>
+
+            <!-- Header -->
+            <tr>
+                <td class="tg-baqh" rowspan="2" style="vertical-align: middle;">Date</td>
+                <td class="tg-baqh" colspan="2">Client Group</td>
+                <td class="tg-0lax" colspan="2">Total Account</td>
+                <td class="tg-0lax" colspan="3">Casual Meals</td>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax" colspan="2" rowspan="2" style="vertical-align: middle; text-align: center;">Remark</td>
+            </tr>
+            <tr>
+                <td class="tg-0lax">PHE OSES</td>
+                <td class="tg-0lax">Andromeda</td>
+                <td class="tg-0lax">Onboard Actual</td>
+                <td class="tg-0lax">As Per Contract</td>
+                <td class="tg-0lax">Breakfast</td>
+                <td class="tg-0lax">Lunch</td>
+                <td class="tg-0lax">Dinner</td>
+                <td class="tg-0lax">Supper</td>
+                <td class="tg-0lax">Total</td>
+            </tr>
+
+            <!-- Content -->
+            <tr>
+                <td class="text-center">1</td>
+                <td class="text-center">13</td>
+                <td class="text-center">27</td>
+                <td class="text-center">40</td>
+                <td class="text-center">45</td>
+                <td class="text-center">15</td>
+                <td class="text-center">15</td>
+                <td class="text-center">15</td>
+                <td class="text-center">15</td>
+                <td class="text-center">40</td>
+                <td class="text-center">Remark kiri</td>
+                <td class="text-center">Remark kanan</td>
+            </tr>
+            <tr>
+                <td class="text-center">2</td>
+                <td class="text-center">13</td>
+                <td class="text-center">27</td>
+                <td class="text-center">40</td>
+                <td class="text-center">45</td>
+                <td class="text-center">15</td>
+                <td class="text-center">15</td>
+                <td class="text-center">15</td>
+                <td class="text-center">15</td>
+                <td class="text-center">40</td>
+                <td class="text-center">Remark kiri</td>
+                <td class="text-center">Remark kanan</td>
+            </tr>
+
+            <tr>
+                <td class="text-center">Total</td>
+                <td class="text-center">26</td>
+                <td class="text-center">54</td>
+                <td class="text-center">80</td>
+                <td class="text-center">90</td>
+                <td class="text-center">30</td>
+                <td class="text-center">30</td>
+                <td class="text-center">30</td>
+                <td class="text-center">30</td>
+                <td class="text-center">80</td>
+                <td class="text-center">-</td>
+                <td class="text-center">-</td>
+            </tr>
+
+            <!-- Divider -->
+            <tr style="line-height: 10px; padding: 0;">
+                <td colspan="12"></td>
+            </tr>
+            
+            <!-- Footer -->
+            <tr>
+                <td colspan="3" style="border-top: none; border-right: none;">
+                    <div style="min-height: 100px; text-align: center; margin-top: -15px;">
+                        <p>Prepared By,</p>
+                        <p style="margin-top: 60px;">Name</p>
+                        <p style="margin-top: -15px; margin-bottom: 0px;">Position</p>
+                    </div>
+                </td>
+                <td colspan="3" style="border-top: none; border-right: none; border-left: none;">
+                    <div style="min-height: 100px; text-align: center; margin-top: -15px;">
+                        <p>Checked By,</p>
+                        <p style="margin-top: 60px;">Name</p>
+                        <p style="margin-top: -15px; margin-bottom: 0px;">Position</p>
+                    </div>
+                </td>
+                <td colspan="3" style="border-top: none; border-right: none; border-left: none;">
+                    <div style="margin-left: -50px; min-height: 100px; text-align: center; margin-top: -15px;">
+                        <p>Approved By,</p>
+                        <p style="margin-top: 60px;">Name</p>
+                        <p style="margin-top: -15px; margin-bottom: 0px;">Position</p>
+                    </div>
+                </td>
+                <td colspan="3" style="border-top: none; border-left: none;">
+                    <div style="min-height: 100px; text-align: center; margin-top: -15px;">
+                        <p>Acknowledge By,</p>
+                        <p style="margin-top: 60px;">Name</p>
+                        <p style="margin-top: -15px; margin-bottom: 0px;">Position</p>
+                    </div>
+                </td>
+            </tr>
+
+            <tr style="line-height: 10px; padding: 0;">
+                <td colspan="12" style="text-align: center; background-color: #bcbcbc;">
+                    <span style="font-size: 0.6rem; font-weight: bold;">PT SURYA BUANA TESTARUAYA</span> <br>
+                    <span style="font-size: 0.6rem;">Kompl€k Gading Bukit lndah Blok J No,07 Jalan Bukit Gading Raya RT 018 RW 008 Kelapa Gading.lakarta Utara 142 rt 0</span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </body>
+
 </html>
