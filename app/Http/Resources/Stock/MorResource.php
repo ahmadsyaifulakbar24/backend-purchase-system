8c89bb370f6d4d451d3b4e72e2199177bf3e997d
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2023-11-15 13:54:26 +0700

    add relation product in history

--- app/Http/Resources/Stock/MorResource.php
+++ app/Http/Resources/Stock/MorResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Stock;
 
+use App\Http\Resources\ItemProduct\ItemProductResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,15 @@ class MorResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'location_id' => $this->location_id,
+            'item_product' => new ItemProductResource($this->item_product),
+            'date' => $this->date,
+            'quantity' => $this->quantity,
+            'item_price' => $this->item_price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
