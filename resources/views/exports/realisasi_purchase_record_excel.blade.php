de278f57ae14c853e7b50d02f65fa453570c4af2
faldibule <faldibule@gmail.com>
2024-03-26 14:23:54 +0700

    Add(ui): add UI for export template (summary & purchase record)

--- resources/views/exports/realisasi_purchase_record_excel.blade.php
+++ resources/views/exports/realisasi_purchase_record_excel.blade.php
@@ -1,3 +1,390 @@
-<tr>
-    <td>Test</td>
-</tr>
+<style>
+    .signature-border {
+        border: solid black 1px;
+        padding: 0px;
+    }
+    table, th, td {
+      border: 1px solid black;
+      padding: 6px;
+      border-collapse: collapse;
+      text-align: center;
+    }
+    td {
+        width: 200;
+        font-size: 0.8rem;
+    }
+    th {
+        width: 200;
+        font-size: 0.8rem;
+        font-weight: bold;
+    }
+    .text-center {
+        text-align: center;
+    }
+    .br-none {
+        border-right: none;
+    }
+    .bl-none {
+        border-left: none;
+    }
+    .bx-none {
+        border-right: none;
+        border-left: none;
+    }
+    .tr-h-100 {
+        height: 80;
+    }
+    .divider {
+        height: 20;
+        border: none;
+    }
+    .text-header {
+        font-size: 1.5rem;
+    }
+    .text-subheader {
+        font-size: 1.3rem;
+    }
+    .bg-blue {
+        background-color: #8EA9DB;
+        color: white
+    }
+    .bg-yellow {
+        background-color: #FFFF00;
+    }
+    .bg-orange {
+        background-color: #F4B084;
+    }
+    .bg-black {
+        background-color: #757171;
+        color: white;
+    }
+    .bg-grey {
+        background-color: #A6A6A6;
+    }
+</style>
+<table border="1">
+    <tr class='tr-h-100'>
+        <td colspan="1" class="br-none">
+            <img src="" alt="checklist">
+        <td colspan="7" class="bx-none">
+            <div>
+                <span class="text-header">PT. SURYA <span style="color: red">BUANA</span> LESTARIJAYA</span>
+                <br /> 
+                <span>Catering & Accomodation Services</span>
+            </div>
+        </td>
+        <td colspan="1" class="bl-none"></td>
+    </tr>
+    <tr class="divider">
+        <td colspan="9"></td>
+    </tr>
+    <tr class="bg-blue">
+        <td colspan="9" class="text-subheader">
+            REALISASI PURCHASE RECORD
+            <br /> 
+            PERIOD : Juli - 2023
+        </td>
+    </tr>
+    <tr class="divider">
+        <td colspan="9"></td>
+    </tr>
+    <tr class="bg-orange">
+        <th>SHIPMENT</th>
+        <th>WEEK</th>
+        <th>SUPPLIER</th>
+        <th>FEDERAL</th>
+        <th>SHIP-114</th>
+        <th>SHIP-115</th>
+        <th>SHIP-112</th>
+        <th>JINDI</th>
+        <th>AMOUNt</th>
+    </tr>
+    <tbody>
+        <!-- Week 1 -->
+        <tr>
+            <td rowspan="4">WEEK</td>
+            <td rowspan="4">I</td>
+        </tr>
+        <tr>
+            <td>Supplier 1</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 2</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 3</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <!-- Divider -->
+        <tr class="divider">
+            <td colspan="8"></td>
+            <td></td>
+        </tr>
+
+        <!-- Week 2 -->
+        <tr>
+            <td rowspan="4">WEEK</td>
+            <td rowspan="4">II</td>
+        </tr>
+        <tr>
+            <td>Supplier 1</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 2</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 3</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <!-- Divider -->
+        <tr class="divider">
+            <td colspan="8"></td>
+            <td></td>
+        </tr>
+
+        <!-- Week 3 -->
+        <tr>
+            <td rowspan="4">WEEK</td>
+            <td rowspan="4">III</td>
+        </tr>
+        <tr>
+            <td>Supplier 1</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 2</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 3</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <!-- Divider -->
+        <tr class="divider">
+            <td colspan="8"></td>
+            <td></td>
+        </tr>
+
+        <!-- Week 5 -->
+        <tr>
+            <td rowspan="4">WEEK</td>
+            <td rowspan="4">IV</td>
+        </tr>
+        <tr>
+            <td>Supplier 1</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 2</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 3</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <!-- Divider -->
+        <tr class="divider">
+            <td colspan="8"></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td rowspan="4">WEEK</td>
+            <td rowspan="4">V</td>
+        </tr>
+        <tr>
+            <td>Supplier 1</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 2</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Supplier 3</td>
+            <td>123.123.123</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <!-- Divider -->
+        <tr class="divider">
+            <td colspan="8"></td>
+            <td></td>
+        </tr>
+
+        <!-- Menghitung Total -->
+        <tr class="bg-yellow">
+            <td></td>
+            <td></td>
+            <td>MENGHITUNG TOTAL</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        
+         <!-- Divider -->
+        <tr style="height: 40px;" class="divider">
+            <td colspan="9"></td>
+        </tr>
+        <tr>
+            <td colspan="2" class="br-none"></td>
+
+            <!-- Prepared By -->
+            <td class="bx-none">
+                <div class="signature-border bg-grey">
+                    <div style="padding: 5px;">Prepared By</div>
+                    <div style="height: 70px; background-color: white;">
+
+                    </div>
+                    <div style="padding: 5px; height: 50px;">
+                        Purchasing
+                        <br />
+                        Haikal Fajar
+                    </div>
+                </div>
+            </td>
+
+            <!-- Checked By -->
+            <td class="bx-none">
+                <div class="signature-border bg-grey">
+                    <div style="padding: 5px;">Checked By</div>
+                    <div style="height: 70px; background-color: white;">
+
+                    </div>
+                    <div style="padding: 5px; height: 50px;">
+                        Quality Control
+                        <br />
+                        Johan
+                    </div>
+                </div>
+            </td>
+
+            <!-- Approved By -->
+            <td class="bx-none">
+                <div class="signature-border bg-grey">
+                    <div style="padding: 5px;">Approved By</div>
+                    <div style="height: 70px; background-color: white;">
+
+                    </div>
+                    <div style="padding: 5px; height: 50px;">
+                        Operation
+                        <br />
+                        Rifki
+                    </div>
+                </div>
+            </td>
+            <!-- Approved By -->
+            <td class="bx-none">
+                <div class="signature-border bg-grey">
+                    <div style="padding: 5px;">Approved By</div>
+                    <div style="height: 70px; background-color: white;">
+
+                    </div>
+                    <div style="padding: 5px; height: 50px;">
+                        Operation
+                        <br />
+                        Dian Ane Wibowo
+                    </div>
+                </div>
+            </td>
+
+            <!-- Ackonwledge By -->
+            <td class="bx-none">
+                <div class="signature-border bg-grey">
+                    <div style="padding: 5px;">Ackonwledge By</div>
+                    <div style="height: 70px; background-color: white;">
+
+                    </div>
+                    <div style="padding: 5px; height: 15px; height: 50px;">
+                        <br />
+                    </div>
+                </div>
+            </td>
+            <td colspan="2" class="bl-none"></td>
+        </tr>
+        <tr style="height: 40px;" class="divider">
+            <td colspan="9"></td>
+        </tr>
+
+    </tbody>
+</table>
--- resources/views/exports/summary_excel.blade.php
+++ resources/views/exports/summary_excel.blade.php
@@ -1,3 +1,110 @@
-<tr>
-    <td>Test</td>
-</tr>
+<style>
+    table, th, td {
+      border: 1px solid black;
+      padding: 6px;
+      border-collapse: collapse;
+      text-align: center;
+    }
+    td {
+        width: 120;
+        font-size: 0.8rem;
+    }
+    th {
+        width: 120;
+        font-size: 0.8rem;
+        font-weight: bold;
+    }
+    .text-center {
+        text-align: center;
+    }
+    .br-none {
+        border-right: none;
+    }
+    .bl-none {
+        border-left: none;
+    }
+    .bx-none {
+        border-right: none;
+        border-left: none;
+    }
+    .tr-h-100 {
+        height: 80;
+    }
+    .divider {
+        height: 30;
+        border: none;
+    }
+    .text-header {
+        font-size: 1.5rem;
+    }
+    .text-subheader {
+        font-size: 1.3rem;
+    }
+    .bg-green {
+        background-color: #A9D08E;
+    }
+    .bg-orange {
+        background-color: #F4B084;
+    }
+    .bg-black {
+        background-color: #757171;
+        color: white;
+    }
+</style>
+<table border="1">
+    <tr class='tr-h-100'>
+        <td colspan="1" class="br-none">
+            <img src="" alt="checklist">
+        <td colspan="3" class="bx-none">
+            <div>
+                <span class="text-header">PT. SURYA BUANA LESTARIJAYA</span>
+                <br /> 
+                <span>Catering & Accomodation Services</span>
+            </div>
+        </td>
+        <td colspan="1" class="bl-none"></td>
+    </tr>
+    <tr class="divider">
+        <td colspan="5"></td>
+    </tr>
+    <tr class="bg-green">
+        <td colspan="5" class="text-subheader">SUMMARY PHYSICAL INVENTORY <br /> Juli - 2023</td>
+    </tr>
+    <tr class="divider">
+        <td colspan="5"></td>
+    </tr>
+    <tr class="bg-orange">
+        <th>NO</th>
+        <th>VESSEL</th>
+        <th>MONTH</th>
+        <th>AMOUNT</th>
+        <th>DATE OF INVENTORY</th>
+    </tr>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>FEDERAL II</td>
+            <td>JULI-2023</td>
+            <td>38.949.282</td>
+            <td>date inventory</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>JINDI</td>
+            <td>JULI-2023</td>
+            <td>38.949.282</td>
+            <td>date inventory</td>
+        </tr>
+        <tr class="bg-black">
+            <td></td>
+            <td>TOTAL</td>
+            <td></td>
+            <td>38.949.282</td>
+            <td></td>
+        </tr>
+    </tbody>
+    <tr style="height: 150;" class="divider">
+        <td colspan="5"></td>
+    </tr>
+
+</table>
