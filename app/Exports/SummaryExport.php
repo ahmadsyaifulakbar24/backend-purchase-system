0a9f16d9f7338032783b9a69260244e87237a4ca
faldibule <faldibule@gmail.com>
2024-05-28 13:34:32 +0700

    Add(styling): add styling at summary excel

--- app/Exports/SummaryExport.php
+++ app/Exports/SummaryExport.php
@@ -7,17 +7,19 @@ use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class SummaryExport implements FromView, ShouldAutoSize, WithStyles
 {
-    protected $year, $month_name, $data;
+    protected $year, $month_name, $data, $data_length;
 
     public function __construct($year, $month_name, $data)
     {
         $this->year = $year;
         $this->month_name = $month_name;
         $this->data = $data;
+        $this->data_length = strval(count($data) + 10);
     }
 
     public function styles(Worksheet $sheet) 
@@ -26,6 +28,42 @@ class SummaryExport implements FromView, ShouldAutoSize, WithStyles
             'alignment' => [
                 'horizontal' => Alignment::HORIZONTAL_CENTER,
                 'vertical' => Alignment::VERTICAL_CENTER,
+            ],
+        ]);
+        $sheet->getStyle('A6:E7')->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'A9D08E',
+                ],
+            ],
+            'font' => [
+                'bold' => true,
+            ],
+        ]);
+        $sheet->getStyle('A9:E9')->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'F4B084',
+                ],
+            ],
+            'font' => [
+                'bold' => true,
+            ],
+        ]);
+        $sheet->getStyle('A'. $this->data_length. ':E'. $this->data_length)->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => '757171',
+                ],
+            ],
+            'font' => [
+                'bold' => true,
+                'color' => [ 
+                    'rgb' => 'FFFFFF',
+                ],
             ],
         ]);
     }
