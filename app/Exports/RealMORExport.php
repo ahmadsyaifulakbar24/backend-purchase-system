61e3eb8e08d77252620b9f4b6a19e1d1262117e3
faldibule <faldibule@gmail.com>
2024-06-06 14:02:04 +0700

    Add(styling): add styling at RPR & MOR export excel

--- app/Exports/RealMORExport.php
+++ app/Exports/RealMORExport.php
@@ -7,6 +7,7 @@ use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class RealMORExport implements FromView, ShouldAutoSize, WithStyles
@@ -20,14 +21,47 @@ class RealMORExport implements FromView, ShouldAutoSize, WithStyles
 
     public function styles(Worksheet $sheet) 
     {
-        return [
-            2 => [
-                'alignment' => [
-                    'horizontal' => 'center',
-                    'vertical' => 'center',
-                ]
-            ]
-        ];
+        $sheet->getStyle('A2:A7')->applyFromArray([
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+                'vertical' => Alignment::VERTICAL_CENTER,
+            ],
+            'font' => [
+                'bold' => true,
+                'size' => '18'
+            ],
+        ]);
+        $sheet->getStyle('A2')->applyFromArray([
+            'font' => [
+                'bold' => true,
+                'size' => '20'
+            ],
+        ]);
+        $sheet->getStyle('A3')->applyFromArray([
+            'font' => [
+                'bold' => true,
+                'size' => '14'
+            ],
+        ]);
+        $sheet->getStyle('A5:A6')->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => '8EA9DB',
+                ],
+            ],
+            'font' => [
+                'bold' => true,
+                'color' => [ 
+                    'rgb' => 'FFFFFF',
+                ],
+            ],
+        ]);
+        $sheet->getStyle(8)->applyFromArray([
+            'font' => [
+                'bold' => true,
+            ],
+        ]);
     }
 
     public function view(): View
--- app/Exports/RealisasiPurchaseRecord.php
+++ app/Exports/RealisasiPurchaseRecord.php
@@ -7,6 +7,7 @@ use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class RealisasiPurchaseRecord implements FromView, ShouldAutoSize, WithStyles
@@ -20,10 +21,45 @@ class RealisasiPurchaseRecord implements FromView, ShouldAutoSize, WithStyles
 
     public function styles(Worksheet $sheet) 
     {
-        $sheet->getStyle('A2')->applyFromArray([
+        $sheet->getStyle('A2:A7')->applyFromArray([
             'alignment' => [
                 'horizontal' => Alignment::HORIZONTAL_CENTER,
                 'vertical' => Alignment::VERTICAL_CENTER,
+            ],
+            'font' => [
+                'bold' => true,
+                'size' => '18'
+            ],
+        ]);
+        $sheet->getStyle('A2')->applyFromArray([
+            'font' => [
+                'bold' => true,
+                'size' => '20'
+            ],
+        ]);
+        $sheet->getStyle('A3')->applyFromArray([
+            'font' => [
+                'bold' => true,
+                'size' => '14'
+            ],
+        ]);
+        $sheet->getStyle('A5:A6')->applyFromArray([
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => '8EA9DB',
+                ],
+            ],
+            'font' => [
+                'bold' => true,
+                'color' => [ 
+                    'rgb' => 'FFFFFF',
+                ],
+            ],
+        ]);
+        $sheet->getStyle(8)->applyFromArray([
+            'font' => [
+                'bold' => true,
             ],
         ]);
     }
